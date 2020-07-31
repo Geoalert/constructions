fc493b6f58368f19f9d7858e0bf34912debd7d77
dqunbp <dqunbp@gmail.com>
2020-07-31 16:01:31 +0300

    fix rasters url

--- src/cities.js
+++ src/cities.js
@@ -10,14 +10,14 @@ export const cities = [
           [61.360359191894524, 55.13826611409422],
           [61.360359191894524, 55.223344564576664],
           [61.23882293701172, 55.223344564576664],
-          [61.23882293701172, 55.13826611409422]
-        ]
-      ]
+          [61.23882293701172, 55.13826611409422],
+        ],
+      ],
     },
     years: ["2010", "2014", "2015", "2016", "2017", "2018", "2019"],
     raster_template:
-      "https://rasters.aeronetlab.space/api/v0/cogs/tiles/{z}/{x}/{y}.png?uri=s3://demos/constructions/chelyabinsk/{year}.tif",
+      "https://staging.aeronetlab.space/rasters/api/v0/cogs/tiles/{z}/{x}/{y}.png?uri=s3://demos/constructions/chelyabinsk/{year}.tif",
     features:
-      "https://minio.aeronetlab.space/public/constructions/chelyabinsk/data.geojson"
-  }
+      "https://minio.aeronetlab.space/public/constructions/chelyabinsk/data.geojson",
+  },
 ];
